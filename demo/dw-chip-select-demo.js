72139ecf135e889b6eda67a61d000d68a8d340c4
Hiten Bhayani <99726733+hitendreamworld@users.noreply.github.com>
2023-04-05 18:57:53 +0530

    feat: create chip (#2) [ci skip]

--- demo/dw-chip-select-demo.js
+++ demo/dw-chip-select-demo.js
@@ -1,12 +1,34 @@
-import { html, LitElement } from "lit";
+import { ThemeStyle } from "@dreamworld/material-styles/theme.js";
+import { css, html, LitElement } from "lit";
 import "../dw-chip-select.js";
-
-const componentName = "dw-chip-select-demo";
+import "../dw-chip.js";
 
 export class DwChipSelectDemo extends LitElement {
+  static get styles() {
+    return [
+      ThemeStyle,
+      css`
+        dw-chip {
+          margin-top: 16px;
+          margin-right: 8px;
+        }
+
+        .wrapper {
+          display: flex;
+        }
+      `,
+    ];
+  }
   render() {
-    return html`<dw-chip-select></dw-chip-select>`;
+    return html`
+      <dw-chip-select></dw-chip-select> <br />
+      <div class="wrapper">
+        <dw-chip .value=${"Chip View"}></dw-chip>
+
+        <dw-chip .value=${"Selected"} selected></dw-chip>
+      </div>
+    `;
   }
 }
 
-customElements.define(componentName, DwChipSelectDemo);
+customElements.define("dw-chip-select-demo", DwChipSelectDemo);
---
+++ dw-chip.js
@@ -0,0 +1,110 @@
+import "@dreamworld/dw-icon";
+import "@dreamworld/dw-ripple";
+import { css, html, LitElement, nothing } from "lit";
+
+// Styles
+import { body2 } from "@dreamworld/material-styles/typography-literals.js";
+
+const Types = {
+  filter: "filter",
+  choice: "choice",
+  input: "input",
+};
+
+export class DwChip extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        position: relative;
+        display: flex;
+        align-items: center;
+        padding-left: 12px;
+        padding-right: 12px;
+        height: var(--dw-chip-height, 32px);
+        border-radius: calc(var(--dw-chip-height, 32px) / 2);
+        ${body2};
+
+        overflow: hidden;
+        box-sizing: border-box;
+      }
+
+      :host(:not([selected])) {
+        border: 1px solid var(--mdc-theme-divider-color, rgba(0, 0, 0, 0.12));
+      }
+
+      :host([selected]) {
+        color: var(--mdc-theme-primary, #02afcd);
+      }
+
+      :host([selected]) dw-icon {
+        padding-right: 8px;
+        --dw-icon-color: var(--mdc-theme-primary, #02afcd);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Determines the type of chip view to be rendered.
+       * Possible values: 'filter', 'choice', and 'input'
+       * Default value: 'filter'
+       */
+      type: { type: String },
+
+      /**
+       * Indicates whether the chip is selected or not.
+       */
+      selected: { type: Boolean },
+
+      /**
+       * Indicates whether the chip is activated or not.
+       */
+      activated: { type: Boolean },
+
+      /**
+       * Specifies the value to be shown as text in the chip.
+       */
+      value: { type: String },
+
+      /**
+       * Indicates whether the chip is disabled or not.
+       */
+      disabled: { type: Boolean },
+
+      /**
+       * Specifies the material icon name to be shown as the leading icon in the chip.
+       * Default: 'done'
+       */
+      icon: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.icon = "done";
+    this.type = Types.filter;
+  }
+
+  render() {
+    return html`
+      <dw-ripple
+        primary
+        ?activated=${this.activated}
+        ?selected=${this.selected}
+        ?disabled=${this.disabled}
+      ></dw-ripple>
+      ${this._renderLeadingIcon}
+      <div class="value">${this.value}</div>
+    `;
+  }
+
+  get _renderLeadingIcon() {
+    if (this.selected && this.type === Types.filter) {
+      return html`<dw-icon name=${this.icon}></dw-icon>`;
+    }
+    return nothing;
+  }
+}
+
+customElements.define("dw-chip", DwChip);
